0acea2561a52dbee2f40bdb8c62f3a48acdef9ac
vika3110 <viktoriia.kokuienko@gmail.com>
2018-05-12 16:37:55 -0400

    Hometask 1

--- programming_fundamentals/homework/basics/2_max_for.py
+++ programming_fundamentals/homework/basics/2_max_for.py
@@ -16,14 +16,13 @@ maximum = 0
 # Find the maximum value from `data` list using `for` loop.
 # Save maximum value from `data` list into `maximum` variable.
 # ADD YOUR CODE HERE.
-for el in data:
-    i = data.index(el)
-    if data[i] > data[i+1]:
-        data[i],data[i+1] = data[i+1],data[i]
-        maximum = data[-1]
-        print("maximum = ", str(maximum))
-        if maximum == max(data):
-            break
+
+for num in data:
+	if num > maximum:
+		maximum = num
+
+print("Maximum value in the list: {}".format(maximum))
+
 # DON'T MODIFY THESE LINES.
 assert maximum == max(data), message
 print("Maximum value has been found! Congratulations!")
--- programming_fundamentals/homework/basics/3_steps_while.py
+++ programming_fundamentals/homework/basics/3_steps_while.py
@@ -15,9 +15,9 @@ step_count = 0
 # Increase `step_count` variable by 1 in each loop.
 # `step_count` should contains number of iterations.
 # ADD YOUR CODE HERE.
-step_count = 0
-while step_count != NUMBER_OF_STEPS:
-    step_count += 1
+
+while step_count < NUMBER_OF_STEPS:
+	step_count += 1
 
 # DON'T MODIFY THESE LINES.
 print("Iterated {} times.".format(step_count))
