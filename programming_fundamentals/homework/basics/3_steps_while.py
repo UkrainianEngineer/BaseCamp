103f176e730cd3749e2f3d4949fa4dcaf3aff6f9
Yuriy Zhuk <yuriy.zhuk@ericsson.com>
2018-05-12 21:02:44 +0300

    While loop

--- programming_fundamentals/homework/basics/3_steps_while.py
+++ programming_fundamentals/homework/basics/3_steps_while.py
@@ -16,6 +16,9 @@ step_count = 0
 # `step_count` should contains number of iterations.
 # ADD YOUR CODE HERE.
 
+while step_count < NUMBER_OF_STEPS:
+    step_count = step_count + 1
+    print(step_count)
 
 # DON'T MODIFY THESE LINES.
 print("Iterated {} times.".format(step_count))
