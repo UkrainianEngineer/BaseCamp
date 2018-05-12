1db22d8c3a3dfe009f292dba92fc0241a8032765
AnyaShymanska <ani4ka30@gmail.com>
2018-05-12 16:34:05 +0300

    add my homework

---
+++ programming_fundamentals/homework/my_basics_homework/1_decimal_to_binary.py
@@ -0,0 +1,19 @@
+"""
+This module describes the home task related to numerical systems.
+It might be useful for both students and mentors.
+"""
+
+__author__ = "Pavlo Ivanchyshyn"
+__maintainer__ = "Pavlo Ivanchyshyn"
+__email__ = "p.ivanchyshyn@gmail.com"
+
+# This task is just for manual training.
+# You should solve this task manually and verify the results using this script.
+birthdate = int(input("Please enter a decimal year of your birth: "))
+binary_date = input("Please enter your binary representation of decimal year of birth:")
+assert "{0:b}".format(birthdate) == str(binary_date), "Invalid answer. Please check your binary representation."
+print("=" * 60)
+print("Congratulations! You've done it!")
+print("=" * 60)
+# my birthdate = 1993
+#my binary_date = 11111001001
