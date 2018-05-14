5824534ab9ce0d2d225a420179cba1ad9b672c12
Yuriy Zhuk <yuriy.zhuk@ericsson.com>
2018-05-14 11:21:56 +0300

    Coffee preparation example

--- programming_fundamentals/homework/basics/0_decimal_to_binary.py
+++ programming_fundamentals/homework/basics/0_decimal_to_binary.py
@@ -9,6 +9,7 @@ __email__ = "p.ivanchyshyn@gmail.com"
 
 # This task is just for manual training.
 # You should solve this task manually and verify the results using this script.
+my_brth = '11111001001'
 birthdate = int(input("Please enter a decimal year of your birth: "))
 binary_date = input("Please enter your binary representation of decimal year of birth:")
 assert "{0:b}".format(int(birthdate)) == str(binary_date), "Invalid answer. Please check your binary representation."
