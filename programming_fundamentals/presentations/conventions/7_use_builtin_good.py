04f916ce48154bb022c735cfa2f016e8ddaf26c0
Pavlo Ivanchyshyn <p.ivanchyshyn@gmail.com>
2018-05-15 07:57:05 +0300

    Added information about conventions class.

---
+++ programming_fundamentals/presentations/conventions/7_use_builtin_good.py
@@ -0,0 +1,17 @@
+"""
+This module describes good examples of built-in-like Python's functions.
+"""
+
+__author__ = "Pavlo Ivanchyshyn"
+__maintainer__ = "Pavlo Ivanchyshyn"
+__email__ = "p.ivanchyshyn@gmail.com"
+
+
+class MyObject():pass
+class Map():pass
+
+obj = MyObject()
+map_obj = Map()
+zip_code = 90213
+id_ = 34
+pk = 34  # This is an alternative solution for ID.
