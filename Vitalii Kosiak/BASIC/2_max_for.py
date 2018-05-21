b9328ae200dd2dde058c7cd99ff1bc7b03519fee
Vitalii Kosiak <viths@i.ua>
2018-05-22 00:03:03 +0300

    glaqahw

---
+++ Vitalii Kosiak/BASIC/2_max_for.py
@@ -0,0 +1,18 @@
+#try:
+ #   leng = int(input("Please input lengh of array of number \n"))
+#except:
+#    print("Please use number 1,2...9")
+
+#x = str(leng)
+#if x[0] == '0' :
+#    print("Error!")
+
+leng = 5
+array = [0, 0, 0, 0, 0]
+a = 0
+while a < leng :
+    array[a] = int(input("input number \n"))
+    a +=1
+
+    
+
