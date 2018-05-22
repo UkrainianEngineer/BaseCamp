94e96191f01b09da8a7975458d5cdf8b11c95860
Iryna Pavlyk <iryna.pavlyk@ericsson.com>
2018-05-22 18:42:34 +0300

    bad_into_good+solutions

--- programming_fundamentals/homework/conventions/bad_into_good.py
+++ programming_fundamentals/homework/conventions/bad_into_good.py
@@ -29,6 +29,10 @@ t = "04/20/2011"
 # Your solution should be added here.
 # It should be applicable for all 3 examples.
 
+color_name = "green"
+is_true = False
+use_python_years = 20
+first_python_data = "04/20/2011"
 
 # Example 4.
 def do_something():pass
@@ -39,3 +43,20 @@ except:
     pass
 
 # Your solution should be added here.
+
+
+def save_money(money):
+    """
+    This function can help you to calculate your saving money
+
+    Args:    money - this is the amount you want to save;
+                   should be int
+    return: savings - the whole amount of savings
+    """
+    savings = 0
+    try:
+        savings += money
+    except (TypeError, ValueError):
+        raise Exception("Incorrect type or value of your money")
+    print("You'he managed to save {}$ ".format(savings))
+    return savings
