a11a4c62f024d4638b141a049fd108891d482280
Yuriy Zhuk <yurakekc@hotmail.com>
2018-05-22 14:59:50 +0300

    Task with decorator

--- programming_fundamentals/homework/programming_principles/decorator.py
+++ programming_fundamentals/homework/programming_principles/decorator.py
@@ -1,6 +1,7 @@
 """
 This module describes a homework related to decorator topic.
 """
+import time
 
 __author__ = "Pavlo Ivanchyshyn"
 __maintainer__ = "Pavlo Ivanchyshyn"
@@ -14,15 +15,21 @@ __email__ = "p.ivanchyshyn@gmail.com"
 # or any specific executions of this function.
 # JUST MODIFY `custon_decorator` decorator.
 
-def custom_decorator():
+def custom_decorator(test_decorator):
     # ADD YOUR CODE HERE.
-    # You are also able to add some parameters if needed.
-    pass
+    def timestamp_func():
+        start_time = time.time()
+        test_decorator()
+        print("Program has executed: %s " % (time.time() - start_time))
+
+    return timestamp_func
 
 
 @custom_decorator
 def test_decorator():
+    time.sleep(5)
     print("This function executes...")
+
 
 test_decorator()
 test_decorator()
