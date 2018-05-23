f51772603d3a9c112ce57721ad0b0abf7b460004
Sergii Menchuk <sergii.menchuk@globallogic.com>
2018-05-23 20:31:42 +0300

    Parsers: DONE; Parser class: NOT DONE

--- programming_fundamentals/homework/4_oop/read_configs.py
+++ programming_fundamentals/homework/4_oop/read_configs.py
@@ -11,34 +11,55 @@ __email__ = "p.ivanchyshyn@gmail.com"
 # It should be able to parse *.ini, *.json, *.yaml and *.xml files.
 # You should implement an appropriate classes for each configuration format.
 
+import xmltodict
+import configparser
+import yaml
+import json
 
 class XmlParser:
-    pass
-
+    def parse(self, filename):
+        with open(filename) as data:
+            config = xmltodict.parse(data.read())
+            # http://docs.python-guide.org/en/latest/scenarios/xml/
+        return config
 
 class IniParser:
-    pass
+    def parse(self, filename):
+        # https://stackoverflow.com/questions/3220670/read-all-the-contents-in-ini-file-into-dictionary-with-python
+        pars = configparser.ConfigParser()
+        pars.read(filename)
+        config = pars.__dict__['_sections'].copy()
+        return config
 
 
 class YamlParser:
-    pass
+    def parse(self, filename):
+        with open(filename, 'r') as data:
+            config = (yaml.load(data))
+        return config
 
 
 class JsonParser:
-    pass
+    def parse(self, filename):
+        with open(filename) as data:
+            config = json.loads(data.read())
+        return config
 
 
-class Parser:
-    """
-    You should implement this class.
-    """
-    pass
+class Parser(XmlParser, IniParser, YamlParser, JsonParser):
+    def parse(self, filename):
+        super(Parser, self).parse(filename)
+
 
 # The following code should works fine.
 # Each `parse` method should return a dict object
 # with configuration parsed from the configuration file.
 parser = Parser()
 xml_data = parser.parse("config.xml")
+print(xml_data)
 ini_data = parser.parse("config.ini")
+print(ini_data)
 yaml_data = parser.parse('config.yaml')
+print(yaml_data)
 json_data = parser.parse('config.json')
+print(json_data)
