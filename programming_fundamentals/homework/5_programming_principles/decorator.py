a05dd149f485f8049527f80b9291b4ba92de9e71
Sergii Menchuk <sergii.menchuk@globallogic.com>
2018-05-24 09:31:06 +0300

    do decorator home task

--- programming_fundamentals/homework/5_programming_principles/decorator.py
+++ programming_fundamentals/homework/5_programming_principles/decorator.py
@@ -6,6 +6,7 @@ __author__ = "Pavlo Ivanchyshyn"
 __maintainer__ = "Pavlo Ivanchyshyn"
 __email__ = "p.ivanchyshyn@gmail.com"
 
+import time
 
 # Create a decorator `custom_decorator` which calculates an execution time
 # of the `test_decorator` function.
@@ -14,15 +15,24 @@ __email__ = "p.ivanchyshyn@gmail.com"
 # or any specific executions of this function.
 # JUST MODIFY `custon_decorator` decorator.
 
-def custom_decorator():
-    # ADD YOUR CODE HERE.
-    # You are also able to add some parameters if needed.
-    pass
+# waiting time to prolong testing function execution
+wait_time = 1.5
+
+
+def custom_decorator(func):
+    def execution_time():
+        start_time = time.time()
+        func()
+        end_time = time.time()
+        print(end_time - start_time)
+    return execution_time
 
 
 @custom_decorator
 def test_decorator():
     print("This function executes...")
+    time.sleep(wait_time)
+
 
 test_decorator()
 test_decorator()
