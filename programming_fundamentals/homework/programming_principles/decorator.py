c1402894a01405895424bb702d6fdb36858a2ec9
AnyaShymanska <ani4ka30@gmail.com>
2018-05-25 20:45:11 +0300

    Add homework (decorator)

---
+++ programming_fundamentals/homework/programming_principles/decorator.py
@@ -0,0 +1,36 @@
+"""
+This module describes a homework related to decorator topic.
+"""
+
+__author__ = "Pavlo Ivanchyshyn"
+__maintainer__ = "Pavlo Ivanchyshyn"
+__email__ = "p.ivanchyshyn@gmail.com"
+
+import datetime
+# Create a decorator `custom_decorator` which calculates an execution time
+# of the `test_decorator` function.
+# After each execution of `test_decorator` function print execution time.
+# You DON'T NEED ANY CHANGES FOR `test_decorator` function
+# or any specific executions of this function.
+# JUST MODIFY `custom_decorator` decorator.
+
+
+
+def custom_decorator(some_function):
+    # ADD YOUR CODE HERE.
+    # You are also able to add some parameters if needed.
+    def wrapper():
+        import datetime
+        start = datetime.datetime.now()
+        some_function()
+        end = datetime.datetime.now()
+        print("Execution time " + str(end - start))
+    return wrapper
+
+
+@custom_decorator
+def test_decorator():
+    print("This function executes...")
+
+test_decorator()
+test_decorator()
