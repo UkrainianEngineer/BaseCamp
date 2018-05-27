b38c0aa5c0eac4ee806985576d4a5d9a5812a506
Bohdan Kaminskyi <b.kaminskyi99@gmail.com>
2018-05-27 23:58:25 +0300

    add oop/decorators homework

--- programming_fundamentals/homework/oop/read_configs.py
+++ programming_fundamentals/homework/oop/read_configs.py
@@ -3,6 +3,12 @@
 This module contains tasks related to object-oriented programming in Python.
 Please read docstrings and complete this task.
 """
+import xml.etree.ElementTree as ET
+import json
+import configparser
+import yaml
+from collections import defaultdict
+
 __author__ = "Pavlo Ivanchyshyn"
 __maintainer__ = "Pavlo Ivanchyshyn"
 __email__ = "p.ivanchyshyn@gmail.com"
@@ -13,26 +19,129 @@ __email__ = "p.ivanchyshyn@gmail.com"
 
 
 class XmlParser:
-    pass
+    @staticmethod
+    def parse(filename):
+        """
+        Function parses passed file into dictionary.
+
+        Args:
+            filename (str): Name of file for reading .
+
+        Returns:
+            dict: Converted from xml-file config.
+        """
+        def tree_to_dict(tree):
+            """
+            Function converts passed tree into dictionary.
+
+            Args:
+                tree (xml.etree.ElementTree): Tree root for parsing.
+
+            Returns:
+                dict: Parsed from tree config.
+            """
+            config = {}
+            for tree_tag in tree.getchildren():
+                if not tree_tag.getchildren():
+                    tree_tag_items = [item.strip() for item in tree_tag.text.split(',')]
+                    if len(tree_tag_items) > 1:
+                        config[tree_tag.tag] = tree_tag_items
+                    else:
+                        config[tree_tag.tag] = tree_tag.text
+                else:
+                    config.update({tree_tag.tag: tree_to_dict(tree_tag)})
+            return config
+        tree = ET.parse(filename).getroot()
+        return tree_to_dict(tree)
 
 
 class IniParser:
-    pass
+    @staticmethod
+    def parse(filename):
+        """
+        Function parses passed file into dictionary.
+
+        Args:
+            filename (str): Name of file for reading .
+
+        Returns:
+            dict: Converted from ini-file config.
+        """
+        config_obj = configparser.ConfigParser()
+        config_obj.read(filename)
+        config = {}
+        for section in config_obj.sections():
+            config.update({section: {}})
+            for key in config_obj.options(section):
+                value = config_obj.get(section, key)
+                value_list = [item.strip() for item in value.split(',')]
+                if len(value_list) > 1:
+                    config[section].update({key: value_list})
+                else:
+                    config[section].update({key: value})
+        return config
 
 
 class YamlParser:
-    pass
+    @staticmethod
+    def parse(filename):
+        """
+        Function parses passed file into dictionary.
+
+        Args:
+            filename (str): Name of file for reading .
+
+        Returns:
+            dict: Converted from yaml-file config.
+        """
+        config = {}
+        try:
+            with open(filename, 'r') as file:
+                try:
+                    config = yaml.load(file)
+                except yaml.YAMLError as exc:
+                    print(exc)
+        except (OSError, IOError):
+            print('File "{}" cannot be opened.'.format(filename))
+        return config
 
 
 class JsonParser:
-    pass
+    @staticmethod
+    def parse(filename):
+        """
+        Function parses passed file into dictionary.
+
+        Args:
+            filename (str): Name of file for reading .
+
+        Returns:
+            dict: Converted from json-file config.
+        """
+        config = {}
+        try:
+            with open(filename, 'r') as file:
+                config = json.load(file)
+        except (OSError, IOError):
+            print('File "{}" cannot be opened.'.format(filename))
+        return config
 
 
 class Parser:
-    """
-    You should implement this class.
-    """
-    pass
+    parsers = {'ini': IniParser,
+               'json': JsonParser,
+               'yaml': YamlParser,
+               'xml': XmlParser}
+
+    def parse(self, filename):
+        extension_position = -1
+        file_extension = filename.split('.')[extension_position]
+        parser = Parser.parsers.get(file_extension)
+        if parser is not None:
+            return parser.parse(filename)
+        else:
+            raise Exception('No suitable parser found for {}'.format(filename))
+
 
 # The following code should works fine.
 # Each `parse` method should return a dict object
--- programming_fundamentals/homework/programming_principles/decorator.py
+++ programming_fundamentals/homework/programming_principles/decorator.py
@@ -1,6 +1,7 @@
 """
 This module describes a homework related to decorator topic.
 """
+import time
 
 __author__ = "Pavlo Ivanchyshyn"
 __maintainer__ = "Pavlo Ivanchyshyn"
@@ -12,17 +13,24 @@ __email__ = "p.ivanchyshyn@gmail.com"
 # After each execution of `test_decorator` function print execution time.
 # You DONT'T NEED ANY CHANGES FOR `test_decorator` function
 # or any specific executions of this function.
-# JUST MODIFY `custon_decorator` decorator.
+# JUST MODIFY `custom_decorator` decorator.
 
-def custom_decorator():
+def custom_decorator(func):
     # ADD YOUR CODE HERE.
     # You are also able to add some parameters if needed.
-    pass
+    def time_wrapper(*args, **kwargs):
+        start_time = time.time()
+        func(*args, **kwargs)
+        end_time = time.time()
+        elapsed_time = end_time-start_time
+        print(elapsed_time)
+    return time_wrapper
 
 
 @custom_decorator
 def test_decorator():
     print("This function executes...")
+
 
 test_decorator()
 test_decorator()
