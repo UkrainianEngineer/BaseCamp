a64808e86e36d9b47621d2bdb365320c00224ae5
Pavlo <pavlo.zablotskyy@gmail.com>
2018-05-27 22:33:47 +0300

    Added decorator function but cannot determine why  diff is shown twice for each call

--- programming_fundamentals/homework/programming_principles/decorator.py
+++ programming_fundamentals/homework/programming_principles/decorator.py
@@ -5,7 +5,7 @@ This module describes a homework related to decorator topic.
 __author__ = "Pavlo Ivanchyshyn"
 __maintainer__ = "Pavlo Ivanchyshyn"
 __email__ = "p.ivanchyshyn@gmail.com"
-
+import datetime
 
 # Create a decorator `custom_decorator` which calculates an execution time
 # of the `test_decorator` function.
@@ -14,15 +14,23 @@ __email__ = "p.ivanchyshyn@gmail.com"
 # or any specific executions of this function.
 # JUST MODIFY `custon_decorator` decorator.
 
-def custom_decorator():
+def custom_decorator(some_func):
     # ADD YOUR CODE HERE.
     # You are also able to add some parameters if needed.
-    pass
+    def wrapper_func(*args, **kwargs):
+        start = datetime.datetime.now()
+        some_func()
+        stop = datetime.datetime.now()
+        diff = stop - start
+        print (diff)
+    return wrapper_func
+
 
 
 @custom_decorator
 def test_decorator():
     print("This function executes...")
 
+test_decorator = custom_decorator(test_decorator)
 test_decorator()
 test_decorator()
