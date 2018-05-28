b6d93b82dba591235af14c3efa00dfc827fa9149
Volodymyr Shkliar <volodyashklyar@gmail.com>
2018-05-28 22:59:18 +0300

    OOP homework.

--- programming_fundamentals/homework/oop/read_configs.py
+++ programming_fundamentals/homework/oop/read_configs.py
@@ -3,6 +3,11 @@
 This module contains tasks related to object-oriented programming in Python.
 Please read docstrings and complete this task.
 """
+import json
+import configparser
+import yaml
+import xml.etree.ElementTree as ET
+
 __author__ = "Pavlo Ivanchyshyn"
 __maintainer__ = "Pavlo Ivanchyshyn"
 __email__ = "p.ivanchyshyn@gmail.com"
@@ -13,26 +18,133 @@ __email__ = "p.ivanchyshyn@gmail.com"
 
 
 class XmlParser:
-    pass
+
+    @staticmethod
+    def parse(filename):
+        """
+        Function implements parsing XMl-file.
+
+        Args:
+            filename (str): File's name for parsing.
+
+        Returns:
+            dict: Parsed XML-file
+        """
+        xml_dict = {}
+
+        tree = ET.parse(filename)
+        root = tree.getroot()
+
+        for child in root:
+            xml_dict[child.tag] = {}
+            for elem in child:
+                elem_list = [item.strip() for item in elem.text.split(',')]
+                if len(elem_list) > 1:
+                    xml_dict[child.tag][elem.tag] = elem_list
+                else:
+                    xml_dict[child.tag][elem.tag] = elem.text
+
+        return xml_dict
 
 
 class IniParser:
-    pass
+
+    @staticmethod
+    def parse(filename):
+        """
+        Function implements parsing ini-file.
+
+        Args:
+             filename (str): File's name for parsing.
+
+        Returns:
+            dict: Parsed ini-file.
+        """
+        config_dict = {}
+
+        config = configparser.ConfigParser()
+        config.read(filename)
+
+        for section_name in config.sections():
+            config_dict[section_name] = {}
+            for key, value in config.items(section_name):
+                value_list = [elem.strip() for elem in value.split(',')]
+                if len(value_list) > 1:
+                    config_dict[section_name][key] = value_list
+                else:
+                    config_dict[section_name][key] = value
+
+        return config_dict
 
 
 class YamlParser:
-    pass
+
+    @staticmethod
+    def parse(filename):
+        """
+        Function implements parsing yaml-file.
+
+        Args:
+            filename (str): File's name for parsing.
+
+        Returns:
+            dict: Parsed yaml-file.
+        """
+        yaml_dict = {}
+        try:
+            with open(filename) as file:
+                yaml_dict = yaml.load(file)
+        except (OSError, IOError) as err:
+            print('Could not read file:', err)
+
+        return yaml_dict
 
 
 class JsonParser:
-    pass
+
+    @staticmethod
+    def parse(filename):
+        """
+        Function implements parsing json-file.
+
+        Args:
+             filename (str): File's name for parsing.
+
+        Returns:
+            dict: Parsed json-file.
+        """
+        json_dict = {}
+        try:
+            with open(filename) as file:
+                json_dict = json.load(file)
+        except (OSError, IOError) as err:
+            print('Could not read file:', err)
+
+        return json_dict
 
 
 class Parser:
     """
     You should implement this class.
     """
-    pass
+    file_types = {
+        'xml': XmlParser,
+        'ini': IniParser,
+        'yaml': YamlParser,
+        'json': JsonParser
+    }
+
+    @staticmethod
+    def parse(filename):
+
+        file_type = filename.split('.')[-1]
+        parser = Parser.file_types.get(file_type)
+
+        try:
+            return parser.parse(filename)
+        except AttributeError as err:
+            print('Error occurred in file \'{}\':'.format(filename), err)
+
 
 # The following code should works fine.
 # Each `parse` method should return a dict object
--- programming_fundamentals/homework/programming_principles/decorator.py
+++ programming_fundamentals/homework/programming_principles/decorator.py
@@ -1,6 +1,7 @@
 """
 This module describes a homework related to decorator topic.
 """
+import time
 
 __author__ = "Pavlo Ivanchyshyn"
 __maintainer__ = "Pavlo Ivanchyshyn"
@@ -14,15 +15,25 @@ __email__ = "p.ivanchyshyn@gmail.com"
 # or any specific executions of this function.
 # JUST MODIFY `custon_decorator` decorator.
 
-def custom_decorator():
+
+def custom_decorator(original_function):
     # ADD YOUR CODE HERE.
     # You are also able to add some parameters if needed.
-    pass
+    def wrapper_function(*args, **kwargs):
+        start = time.time()
+        result = original_function(*args, **kwargs)
+        end = time.time()
+
+        print('Execution time {:f} sec'.format((end - start)))
+
+        return result
+    return wrapper_function
 
 
 @custom_decorator
 def test_decorator():
     print("This function executes...")
+
 
 test_decorator()
 test_decorator()
