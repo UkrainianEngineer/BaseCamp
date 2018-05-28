df4663a503e2ab1cd9e881e65b7f57abb8bd1afe
myroslava <myroslava.hnidets93@gmail.com>
2018-05-28 18:31:48 +0300

    Add homework_5/decorator

--- programming_fundamentals/homework/programming_principles/decorator.py
+++ programming_fundamentals/homework/programming_principles/decorator.py
@@ -14,15 +14,26 @@ __email__ = "p.ivanchyshyn@gmail.com"
 # or any specific executions of this function.
 # JUST MODIFY `custon_decorator` decorator.
 
-def custom_decorator():
-    # ADD YOUR CODE HERE.
-    # You are also able to add some parameters if needed.
-    pass
+import datetime
+
+
+def custom_decorator(some_function):
+
+    def wrapper():
+        start_time = datetime.datetime.now()
+        some_function()
+        end_time = datetime.datetime.now()
+        time_of_execution = str(end_time - start_time)
+
+        return 'This function executes {}.'.format(time_of_execution)
+
+    return wrapper
 
 
 @custom_decorator
 def test_decorator():
     print("This function executes...")
+
 
 test_decorator()
 test_decorator()
