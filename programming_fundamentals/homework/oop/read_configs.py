8b726742cea8ca4ce42cc379859eddc582da1592
Pavlo <pavlo.zablotskyy@gmail.com>
2018-05-29 21:01:20 +0300

    Implemented XmlParser

--- programming_fundamentals/homework/oop/read_configs.py
+++ programming_fundamentals/homework/oop/read_configs.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+import xml.etree.ElementTree
+
 """
 This module contains tasks related to object-oriented programming in Python.
 Please read docstrings and complete this task.
@@ -11,9 +13,16 @@ __email__ = "p.ivanchyshyn@gmail.com"
 # It should be able to parse *.ini, *.json, *.yaml and *.xml files.
 # You should implement an appropriate classes for each configuration format.
 
-
 class XmlParser:
-    pass
+    def parse (self, filename):
+        xml_dict = {}
+        e = xml.etree.ElementTree.parse(filename).getroot()
+        for child in e:
+            inner_dict = {}
+            for grand_child in child:
+                inner_dict[grand_child.tag] = grand_child.text
+            xml_dict[child.tag] = inner_dict
+        return (xml_dict)
 
 
 class IniParser:
@@ -34,11 +43,16 @@ class Parser:
     """
     pass
 
+parser = XmlParser()
+parser.parse("config.xml")
+
+
 # The following code should works fine.
 # Each `parse` method should return a dict object
 # with configuration parsed from the configuration file.
+"""
 parser = Parser()
 xml_data = parser.parse("config.xml")
 ini_data = parser.parse("config.ini")
 yaml_data = parser.parse('config.yaml')
-json_data = parser.parse('config.json')
+json_data = parser.parse('config.json')"""
