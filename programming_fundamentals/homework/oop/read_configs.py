ab806497685c187e83e981efb7f551cd6a96f4a6
Vitalii Kosiak <viths@i.ua>
2018-05-30 18:37:52 +0300

    oop

--- programming_fundamentals/homework/oop/read_configs.py
+++ programming_fundamentals/homework/oop/read_configs.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
+try :
+    import xml.etree.cElementTreex as ET
+except :
+    import xml.etree.ElementTree as ET
+import os
+
 """
 This module contains tasks related to object-oriented programming in Python.
 Please read docstrings and complete this task.
@@ -13,8 +19,17 @@ __email__ = "p.ivanchyshyn@gmail.com"
 
 
 class XmlParser:
-    pass
-
+    def parse (self, filename):
+        main_tree = ET.parse(filename)   #main branch
+        root = main_tree.getroot()
+        xdict = {}
+        for chld in root:
+            ch_dict = {}
+            for g_ch in chld:
+                ch_dict[g_ch.tag] = g_ch.text
+            xdict[chld.tag] = ch_dict
+        return xdict
+    
 
 class IniParser:
     pass
@@ -28,17 +43,40 @@ class JsonParser:
     pass
 
 
-class Parser:
+class Parser(XmlParser, IniParser, YamlParser, JsonParser) :
+    def parse(self, file_name) :
+        
+        if file_name.lower().endswith('xml') == True :
+            parser = XmlParser()
+            return parser.parse(file_name)
+        
+        elif file_name.lower().endswith('ini') == True:
+            parser = IniParser()
+            return parser.parse(file_name)
+        
+        elif file_name.lower().endswith('yaml') == True :
+            parser = YamlParser()
+            return parser.parse(file_name)
+        
+        elif file_name.lower().endswith('json') == True:
+            parser = JsonParser()
+            return parser.parse(file_name)
+        else :
+            print("Unknown type of file!")
     """
     You should implement this class.
     """
-    pass
+    #def parse(self, filename):
+        #parsed_dict = super().parse(filename)
+        #return parsed_dict
 
 # The following code should works fine.
 # Each `parse` method should return a dict object
 # with configuration parsed from the configuration file.
+
 parser = Parser()
 xml_data = parser.parse("config.xml")
-ini_data = parser.parse("config.ini")
-yaml_data = parser.parse('config.yaml')
-json_data = parser.parse('config.json')
+
+#ini_data = parser.parse("config.ini")
+#yaml_data = parser.parse('config.yaml')
+#json_data = parser.parse('config.json')
