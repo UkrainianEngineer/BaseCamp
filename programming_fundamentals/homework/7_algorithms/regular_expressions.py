4e7e688db6adf3ee226bbc349497a23c0020b3c1
Pavlo Ivanchyshyn <p.ivanchyshyn@gmail.com>
2018-05-31 01:43:20 +0300

    Added a class related to Algorithms.

---
+++ programming_fundamentals/homework/7_algorithms/regular_expressions.py
@@ -0,0 +1,39 @@
+"""
+This module describes a homework related to URL validation via regular expressions.
+"""
+
+__author__ = "Pavlo Ivanchyshyn"
+__maintainer__ = "Pavlo Ivanchyshyn"
+__email__ = "p.ivanchyshyn@gmail.com"
+
+
+# 1. First task.
+# Create a regular expression which MATCHES the following URL:
+# https://facebook.com/<username>/allactivity
+#
+# Where <username> - your facebook ID (or something like `zuck`).
+#
+# Your expression should work also for other User's pages, like:
+# https://facebook.com/<username>/friends
+# and so on.
+#
+# Also your expression should work without changes for:
+# http://facebook.com/<username>/allactivity
+# This task should be wrapped into `is_valid_url` function.
+#
+# Example of usage:
+#
+# is_valid_url("https://facebook.com/pivanchy/allactivity")  # Returns `True`.
+# is_valid_url("https://facebook.com/pivanchy")  # Return `False`.
+# is_valid_url("https://facebook.com/pivanchy/allactivity/info")  # Returns `False`.
+# is_valid_url("https://facebook.com/allactivity")  # Returns `False`.'
+#
+# 2. Second task.
+# Using a URLs from the first task, create another regular expression,
+# which helps to SEARCH User's ID from provided URL.
+# This task should be wrapper into `get_user_id` function.
+#
+# Example of usage:
+#
+# get_user_id("http://facebook.com/pivanchy/allactivity")  # Expected output is `pivanchy`.
+# get_user_id("https://facebook.com/pivanchy/allactivity")  # Expected output: `pivanchy`.
