7bef236de36e7b0d0d94cf4f8c463475810a0d69
Iryna Pavlyk <iryna.pavlyk@ericsson.com>
2018-06-01 19:27:25 +0300

    6_ood decorators

--- programming_fundamentals/homework/6_ood/decorators.py
+++ programming_fundamentals/homework/6_ood/decorators.py
@@ -1,3 +1,5 @@
+import datetime
+
 """This module describes homework related to decorators.
    Author: pivanchy.
 """
@@ -12,18 +14,30 @@
 #
 # Example:
 RETRIES = 4
+count = 0
 
 
-def decorator(add_parameters_if_needed):
-    # Use `RETRIES` variable here somehow.
-    # You should add some behaviour here for easier testing.
-    pass
+def decorator(function):
+
+    def func_wrapper(*agrs, **kwargs):
+        global count
+        start_time = datetime.datetime.now()
+        try:
+            function(*agrs, **kwargs)
+        except (TypeError or ValueError):
+            count += 1
+            if count < RETRIES:
+                return func_wrapper()
+        end_time = datetime.datetime.now()
+        print("This function executes {}".format(str(end_time - start_time)))
+
+    return func_wrapper
 
 
 @decorator
-def my_func(dd_parameters_if_needed):
-    # You should add some behaviour here for easier testing.
-    pass
+def my_func():
+    print("This function executes..." + 1)
+
 
 # 2) Improve previous task to make it possible to pass a parameter
 #    into your decorator.
@@ -31,15 +45,32 @@ def my_func(dd_parameters_if_needed):
 # Example:
 
 
-def decorator(dd_parameters_if_needed):
-    # You should add some behaviour here for easier testing.
-    pass
+def decorator(retries):
+    def real_decorator(function):
+        def wrapper(*args, **kwargs):
+            start_time = datetime.datetime.now()
+            for _ in range(retries):
+                try:
+                    func_value = function(*args, **kwargs)
+                    return func_value
+                except (NameError, TypeError or ValueError):
+                    print("An error occured during function execution")
+            end_time = datetime.datetime.now()
+            print("This function executes {}".format(str(end_time - start_time)))
+        return wrapper
+    return real_decorator
 
 
 @decorator(retries=4)
-def my_func(dd_parameters_if_needed):
-    # You should add some behaviour here for easier testing.
-    pass
+def my_func(n):
+    summ = 0
+    for i in range(n):
+        summ += i
+    print("Summ is equal: " + summ1)
+    return summ
+
+    print(my_func(77))
+
 #
 #  3) Implement decorator which caches data.
 #     It means that for first run of function it should print something like:
@@ -48,12 +79,42 @@ def my_func(dd_parameters_if_needed):
 #         `Using data from cache. => <value_from_cache>`
 #
 #  Example:
-#
+
+
+# version 1
+def cached(function):
+
+    def wrapper(*args, **kwargs):
+        wrapper.calls += 1
+        if wrapper.calls > 1:
+            print("Using data from cache. => Hello, world!")
+        else:
+            print("Calculated value. => Hello, world!")
+        return function(*args, **kwargs)
+    wrapper.calls = 0
+    return wrapper
+
+
+# version 2
+def cached(function):
+    cache = {}
+    def wrapper(*args, **kwargs):
+        nonlocal cache
+        value = (*args, tuple(kwargs.items()))
+        if value not in cache :
+            return_value = function(*args, **kwargs)
+            cache[value] = return_value
+            print('Calculated value. => {}'.format(return_value))
+        else:
+            print('Using data from cache. => {}'.format(cache[value]))
+    return wrapper
+
 @cached
 def my_func():
     # Doing something.
     # E.g.:
     return "Hello, world!"
+
 
 my_func() # prints `Calculated value. => Hello, world!`
 my_func() # prints `Using data from cache. => Hello, world!`
