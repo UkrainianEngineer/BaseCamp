03cad9755208f47c9b8378d9b857738a7fbf7c6f
Yuriy Zhuk <yurakekc@hotmail.com>
2018-06-02 13:53:14 +0300

    Sixth homework

--- programming_fundamentals/homework/6_ood/decorators.py
+++ programming_fundamentals/homework/6_ood/decorators.py
@@ -2,6 +2,8 @@
    Author: pivanchy.
 """
 
+from programming_fundamentals.homework.coffee_preparation import Coffee
+
 # Implement `retry` decorator.
 # If some part of code in decorated function fails, try to re-run it again.
 # If decorated function executes successfully, you don't need to re-run it.
@@ -9,53 +11,96 @@
 # 1) add some global variable like `RETRIES = 4`.
 #    Use this variable for your retry decorator.
 #    Failed function should retries up to 4 times.
-#
-# Example:
+
 RETRIES = 4
 
 
-def decorator(add_parameters_if_needed):
-    # Use `RETRIES` variable here somehow.
-    # You should add some behaviour here for easier testing.
-    pass
+def decorator(func):
+    def retry(*a, **k):
+        count = 0
+        while count < RETRIES:
+            try:
+                func(*a, **k)
+                print("I'd do coffee!")
+                return True
+            except (ValueError, TypeError):
+                print("Function failed = " + str(count+1) + "times")
+                count += 1
+        else:
+            raise Exception(mssg="Function failed all times")
+    return retry
 
-
-@decorator
-def my_func(dd_parameters_if_needed):
-    # You should add some behaviour here for easier testing.
-    pass
 
 # 2) Improve previous task to make it possible to pass a parameter
 #    into your decorator.
-#
-# Example:
+@decorator
+def my_func(value):
+    if value > 0:
+        brewer = Coffee()
+        brewer.install_filter()
+        brewer.add_spoons(value)
+        brewer.turn_on()
+        brewer.brew()
+    else:
+        raise ValueError
 
 
-def decorator(dd_parameters_if_needed):
-    # You should add some behaviour here for easier testing.
-    pass
+def decorator(retries):
+    def func_decorator(func):
+        def retry(*a, **k):
+            count = 0
+            while count < retries:
+                try:
+                    func(*a, **k)
+                    print("I'd do coffee!")
+                    return True
+                except ValueError:
+                    print("Function failed = " + str(count+1) + "times")
+                    count += 1
+            else:
+                raise Exception(mssg="Function failed all times")
+        return retry
+    return func_decorator
 
 
 @decorator(retries=4)
-def my_func(dd_parameters_if_needed):
-    # You should add some behaviour here for easier testing.
-    pass
-#
+def my_func(value):
+    if value > 0:
+        brewer = Coffee()
+        brewer.install_filter()
+        brewer.add_spoons(value)
+        brewer.turn_on()
+        brewer.brew()
+    else:
+        raise ValueError
+
+
 #  3) Implement decorator which caches data.
 #     It means that for first run of function it should print something like:
 #         `Calculated value. => <calculated_value>`
 #     For second, third, ... runs it should print:
 #         `Using data from cache. => <value_from_cache>`
-#
-#  Example:
-#
+
+def cached(func, value=None):
+
+    def cache_wrapper():
+        nonlocal value
+        if value is None:
+            value = func()
+            print("Calculated value. => " + str(value))
+        else:
+            print("Using data from cache. => " + str(value))
+        return value
+
+    return cache_wrapper
+
+
 @cached
 def my_func():
-    # Doing something.
-    # E.g.:
     return "Hello, world!"
 
-my_func() # prints `Calculated value. => Hello, world!`
-my_func() # prints `Using data from cache. => Hello, world!`
-my_func() # prints `Using data from cache. => Hello, world!`
-my_func() # prints `Using data from cache. => Hello, world!`
+
+my_func()  # prints `Calculated value. => Hello, world!`
+my_func()  # prints `Using data from cache. => Hello, world!`
+my_func()  # prints `Using data from cache. => Hello, world!`
+my_func()  # prints `Using data from cache. => Hello, world!`
