d22d13860c1a3ef002151cc1e204a27bdb9ecf46
Yuriy Zhuk <yurakekc@hotmail.com>
2018-06-02 16:46:45 +0300

    user permission sixth hometask

--- programming_fundamentals/homework/6_ood/user_permissions.py
+++ programming_fundamentals/homework/6_ood/user_permissions.py
@@ -17,11 +17,41 @@ __email__ = "p.ivanchyshyn@gmail.com"
 # Activity log - text file.
 
 class User:
-    pass
+
+    def __init__(self, name, group):
+        self.name = name
+        self.group = group
+        self.check_page = None
+
+    def set_group(self, new_group):
+        self.group = new_group
+
+    def check_permissions(self, check_page):
+        self.check_page = check_page.page_tittle
+        if self.group in check_page.allowed_groups:
+            return True
+        else:
+            return False
+
+    def login(self):
+        with open('activity_log.txt', 'a+') as file:
+                file.write('User' + self.name + 'has been successfully logged in' + self.check_page + 'page.')
+
+    def logout(self):
+        with open('activity_log.txt', 'a+') as file:
+                file.write('User' + self.name + 'has not enough permissions for' + self.check_page + 'page.')
 
 
 class Page:
-    pass
+
+    def __init__(self, page_tittle):
+        self.page_tittle = page_tittle
+        self.allowed_groups = []
+
+    def allow_for(self, group_list):
+        for i in group_list:
+            self.allowed_groups.append(i)
+
 
 admin_user = User("Pavlo", "admin")
 moderation_user = User("Yura", "moderator")
@@ -32,6 +62,7 @@ page = Page("Settings")
 page.allow_for(["admin", "moderator"])
 
 is_allowed_admin = admin_user.check_permissions(page)
+
 if is_allowed_admin:
     # Login should write a success message into activity log.
     # It might be something similar to:
@@ -41,4 +72,4 @@ else:
     # Logout should write an error message into activity log.
     # It might be something similar to:
     # User `Pavlo` has not enough permissions for `Settings` page.
-    admin_user.logot()
+    admin_user.logout()
