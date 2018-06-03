0623ed4efdcae528df92e46a7eb52a6cf2ca0510
myroslava <myroslava.hnidets93@gmail.com>
2018-06-03 18:41:05 +0300

    Add homework 6_ood / decorators+user_permissions

--- programming_fundamentals/homework/6_ood/decorators.py
+++ programming_fundamentals/homework/6_ood/decorators.py
@@ -2,6 +2,8 @@
    Author: pivanchy.
 """
 
+import datetime
+
 # Implement `retry` decorator.
 # If some part of code in decorated function fails, try to re-run it again.
 # If decorated function executes successfully, you don't need to re-run it.
@@ -9,53 +11,94 @@
 # 1) add some global variable like `RETRIES = 4`.
 #    Use this variable for your retry decorator.
 #    Failed function should retries up to 4 times.
-#
+
 # Example:
 RETRIES = 4
 
 
-def decorator(add_parameters_if_needed):
-    # Use `RETRIES` variable here somehow.
-    # You should add some behaviour here for easier testing.
-    pass
+def decorator(test_function):
+
+    def wrapper():
+        retries_num = RETRIES
+
+        while retries_num > 0:
+            retries_num -= 1
+            try:
+                return test_function()
+            except Exception as e:
+                print(e)
+        print('All {} retries failed.'.format(RETRIES))
+
+    return wrapper
 
 
 @decorator
-def my_func(dd_parameters_if_needed):
-    # You should add some behaviour here for easier testing.
-    pass
+def my_func():
+    some_time = datetime.datetime.now()
+    if some_time.microsecond % 2 == 0:
+        raise Exception('Fail!')
+    else:
+        print('Success!')
+
 
 # 2) Improve previous task to make it possible to pass a parameter
 #    into your decorator.
-#
-# Example:
 
 
-def decorator(dd_parameters_if_needed):
-    # You should add some behaviour here for easier testing.
-    pass
+def decorator(retries):
+    def real_decorator(test_function):
+        def wrapper():
+
+            retries_num = retries
+
+            while retries_num > 0:
+                retries_num -= 1
+                try:
+                    return test_function()
+                except Exception as e:
+                    print(e)
+            print('All {} retries failed.'.format(retries))
+
+        return wrapper
+    return real_decorator
 
 
-@decorator(retries=4)
-def my_func(dd_parameters_if_needed):
-    # You should add some behaviour here for easier testing.
-    pass
-#
+@decorator(retries=2)
+def my_func():
+    some_time = datetime.datetime.now()
+    if some_time.microsecond % 2 == 0:
+        raise Exception('Fail!')
+    else:
+        print('Success!')
+
+
 #  3) Implement decorator which caches data.
 #     It means that for first run of function it should print something like:
 #         `Calculated value. => <calculated_value>`
 #     For second, third, ... runs it should print:
 #         `Using data from cache. => <value_from_cache>`
-#
-#  Example:
-#
+
+def cached(test_function):
+    cash_value = None
+
+    def wrapper():
+        nonlocal cash_value
+
+        if cash_value is None:
+            cash_value = test_function()
+            print('Calculated value. => {}'.format(cash_value))
+        else:
+            print('Using data from cache. => {}'.format(cash_value))
+
+    return wrapper
+
+
 @cached
 def my_func():
-    # Doing something.
-    # E.g.:
     return "Hello, world!"
 
-my_func() # prints `Calculated value. => Hello, world!`
-my_func() # prints `Using data from cache. => Hello, world!`
-my_func() # prints `Using data from cache. => Hello, world!`
-my_func() # prints `Using data from cache. => Hello, world!`
+
+my_func()  # prints `Calculated value. => Hello, world!`
+my_func()  # prints `Using data from cache. => Hello, world!`
+my_func()  # prints `Using data from cache. => Hello, world!`
+my_func()  # prints `Using data from cache. => Hello, world!`
--- programming_fundamentals/homework/6_ood/user_permissions.py
+++ programming_fundamentals/homework/6_ood/user_permissions.py
@@ -16,12 +16,43 @@ __email__ = "p.ivanchyshyn@gmail.com"
 # As a result we should have an activity log with all the information.
 # Activity log - text file.
 
+
 class User:
-    pass
+
+    def __init__(self, user_name, group):
+        self.name = user_name
+        self.group = group
+        self.page_name = None
+
+    def set_group(self, role):
+        self.group = role
+
+    def check_permissions(self, page_object):
+        self.page_name = page_object.page_name
+        permission_list = page_object.permission_list
+
+        if self.group in permission_list:
+            return True
+        return False
+
+    def login(self):
+        with open('activity_log.txt', 'a') as log_file:
+            log_file.write('User {} has been successfully logged in {} page.\n'.format(self.name, self.page_name))
+
+    def logout(self):
+        with open('activity_log.txt', 'a') as log_file:
+            log_file.write('User {} has not enough permissions for {} page.\n'.format(self.name, self.page_name))
 
 
 class Page:
-    pass
+
+    def __init__(self, page_name):
+        self.page_name = page_name
+        self.permission_list = []
+
+    def allow_for(self, permission_list):
+        self.permission_list = permission_list.copy()
+
 
 admin_user = User("Pavlo", "admin")
 moderation_user = User("Yura", "moderator")
@@ -31,7 +62,9 @@ regular_user.set_group("moderator")
 page = Page("Settings")
 page.allow_for(["admin", "moderator"])
 
+
 is_allowed_admin = admin_user.check_permissions(page)
+
 if is_allowed_admin:
     # Login should write a success message into activity log.
     # It might be something similar to:
@@ -41,4 +74,4 @@ else:
     # Logout should write an error message into activity log.
     # It might be something similar to:
     # User `Pavlo` has not enough permissions for `Settings` page.
-    admin_user.logot()
+    admin_user.logout()
