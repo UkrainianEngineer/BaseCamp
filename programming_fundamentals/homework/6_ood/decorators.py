2999f818a323718f7958f5862a72452f71986cc5
AnyaShymanska <ani4ka30@gmail.com>
2018-06-03 18:34:01 +0300

    Add decorators homework

--- programming_fundamentals/homework/6_ood/decorators.py
+++ programming_fundamentals/homework/6_ood/decorators.py
@@ -1,6 +1,7 @@
 """This module describes homework related to decorators.
    Author: pivanchy.
 """
+import random
 
 # Implement `retry` decorator.
 # If some part of code in decorated function fails, try to re-run it again.
@@ -14,16 +15,31 @@
 RETRIES = 4
 
 
-def decorator(add_parameters_if_needed):
+def decorator(func):
     # Use `RETRIES` variable here somehow.
     # You should add some behaviour here for easier testing.
-    pass
+    def wrapper(*args, **kwargs):
+        count = 0
+        try:
+            func(*args, **kwargs)
+            print('Function was successfully executed.')
+        except (ValueError, TypeError):
+            print("Function failed = " + str(count + 1) + "times")
+            count += 1
+    return wrapper
+
+
 
 
 @decorator
-def my_func(dd_parameters_if_needed):
+def my_func():
     # You should add some behaviour here for easier testing.
-    pass
+    random_number = random.random()
+    if random_number < 0.8:
+        raise Exception('Failed run')
+    else:
+        print("Success")
+    return random_number
 
 # 2) Improve previous task to make it possible to pass a parameter
 #    into your decorator.
@@ -31,15 +47,30 @@ def my_func(dd_parameters_if_needed):
 # Example:
 
 
-def decorator(dd_parameters_if_needed):
+def decorator(retries=None):
     # You should add some behaviour here for easier testing.
-    pass
+    def func_decorator(func):
+        def wrapper(*args, **kwargs):
+            count = 0
+            while count > retries:
+                try:
+                    return func()
+                except Exception:
+                    count += 1
+        return wrapper
+    return func_decorator
 
 
 @decorator(retries=4)
-def my_func(dd_parameters_if_needed):
+def my_func():
     # You should add some behaviour here for easier testing.
-    pass
+    random_number = random.random()
+    if random.random() < 0.8:
+        raise Exception('Failed run')
+    else:
+        print("Success")
+    return random_number
+
 #
 #  3) Implement decorator which caches data.
 #     It means that for first run of function it should print something like:
@@ -49,10 +80,24 @@ def my_func(dd_parameters_if_needed):
 #
 #  Example:
 #
+
+
+def cached(func):
+    cache = []
+
+    def decorator(*args, **kwargs):
+        nonlocal cache
+        result = func(*args, **kwargs)
+
+        if id(func) not in cache:
+            cache.append(id(func))
+            return '{} => {}'.format('Calculated value.', result)
+        else:
+            return '{} => {}'.format('Using data from cache.', result)
+    return decorator
+
 @cached
-def my_func():
-    # Doing something.
-    # E.g.:
+def my_cached_func():
     return "Hello, world!"
 
 my_func() # prints `Calculated value. => Hello, world!`
