fd9299141969943e4460b7638b4adf0f99f57b31
Yuriy Zhuk <yurakekc@hotmail.com>
2018-06-09 16:01:22 +0300

    update pow function

--- programming_fundamentals/homework/7_algorithms/recursion.py
+++ programming_fundamentals/homework/7_algorithms/recursion.py
@@ -21,7 +21,25 @@ __email__ = "p.ivanchyshyn@gmail.com"
 # pow(4, 6)  # Returns 4096.
 # And so on.
 def power_function(x, y):
-    if y == 0:
-        return x
-    return x * power_function(x, y-1)
+    #  check our values
+    if isinstance(x, (int, float)) and isinstance(y, int):
+        if x == 0 or x == 1 or y == 1:
+            return x
+        if y == 0:
+            return 1
+        if y > 0:
+            # positive case
+            if y == 2:
+                return x*x
+            else:
+                return x * power_function(x, y-1)
+        elif y < 0:
+            # negative case
+            return 1./power_function(x, -y)
+    else:
+        raise TypeError('Argument must be integer or float')
 
+
+x = int(input("Enter base x: "))
+y = int(input("Enter exponential value y: "))
+print("Result:", pow(x, y))
